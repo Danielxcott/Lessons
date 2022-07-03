8b0051b6a50d8a3cd0398469d9dc9b828a2eb8f9
Danielxcott <changwilliam826@gmail.com>
2022-07-03 21:49:10 +0630

    promise chaining

--- js lesson/promise.js
+++ js lesson/promise.js
@@ -12,18 +12,40 @@ const fetchData = () =>{
         },5000)
     })
 } 
+const errorData = () =>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            reject("this is error");
+        },3000)
+    })
+}
 function finalResult(){
     console.log("here is your final result");
 }
 
+//pyramid of dom  || call back hell
+// login().then((data)=>{
+//     console.log(data);
+//     fetchData().then((data)=>{
+//         console.log("here is "+data);
+//         finalResult();
+//     }).catch((err)=>{
+//         console.log(err)
+//     })
+// }).catch(()=>{
+//     console.log("can't solve");   
+// })
+
+//promise Chaining to avoid callback hell
 login().then((data)=>{
     console.log(data);
-    fetchData().then((data)=>{
-        console.log("here is "+data);
-        finalResult();
-    }).catch((err)=>{
-        console.log(err)
-    })
-}).catch(()=>{
-    console.log("can't solve");   
+    return fetchData();
+})
+.then((fData)=>{
+    console.log(fData);
+    finalResult();
+    return errorData();
+})
+.catch((error)=>{
+    console.log("can't solve the problem"+error);
 })
